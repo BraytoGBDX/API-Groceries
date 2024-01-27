7caccd329ea1722b543ad1ca40de42e538a51901
BraytoGBDX <lculpa45@gmail.com>
2024-01-26 19:12:22 -0600

    v hnc

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,5 +1,8 @@
 import producDAO from "../dao/productsDao.js";
 
+const C = console.log.bind(console.log)
+
+
 export const getAllProducts=(req,res)=>{
     producDAO.getAll()
     .then(result=>{
@@ -8,4 +11,19 @@ export const getAllProducts=(req,res)=>{
     .catch(err=>console.error(err))
 };
 
+export const getOneProduct=(req,res)=>{
+    producDAO.getOne(req.params.barcode)
+    .then(result=>{ 
+        if(result!=null){
+        res.json(result)
+        }else{
+            res.json({
+                status:"Product not found "
+            })
+        }
+    })
+    .catch(err=>res.json({
+        status:"servidor no disponible"
+    }))
+}
 
