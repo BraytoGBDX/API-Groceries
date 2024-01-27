c4326e9c7f8c7803d62ee93ca437aa340128a437
BraytoGBDX <lculpa45@gmail.com>
2024-01-26 19:12:22 -0600

    creando ruta getOne

--- src/dao/productsDao.js
+++ src/dao/productsDao.js
@@ -1,8 +1,15 @@
 import Product from '../models/productModel.js'
+
+const C = console.log.bind(console.log)
 const productDAO={}
 productDAO.getAll = async()=>{
     const products=await Product.find()
     return products
+}
+
+productDAO.getOne = async(barcodeProduct)=>{
+    const product=await Product.findOne({barcode:barcodeProduct})
+    return product
 }
 
 export default productDAO;
